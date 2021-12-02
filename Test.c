6ae13bd7e8702d18a4cc4e6f72f63d466e0f7510
rishabh <rishabh26402@gmail.com>
2021-12-02 22:55:18 +0530

    this is my hello world programme

---
+++ Test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    short int interger = 4;
+    
+   
+
+    printf("the size of taken by short int is %d\n", sizeof(short int));
+    
+    
+    return 0;
+}
